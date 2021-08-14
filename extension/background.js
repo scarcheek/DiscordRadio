91739b10e761783b499df021e4340708f60eca0e
Loris <scarcheekttgm@gmail.com>
2021-08-14 03:23:29 +0200

    You can now change the tracked website and when you close the tab, the track gets removed

--- extension/background.js
+++ extension/background.js
@@ -9,10 +9,20 @@ chrome.contextMenus.create({
 
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
     if (info.menuItemId == "some-command") {
+        if (selectedTabId && selectedTabId !== tab.id) {
+            chrome.tabs.sendMessage(selectedTabId, { type: "tabRemove" }, function (response) {
+                console.log(`Stopped tracking tab with id: ${selectedTabId}`)
+            });
+        }
+
         selectedTabId = tab.id
         chrome.tabs.sendMessage(tab.id, { type: "init" }, function (response) {
-            console.log(`Now tracking: ${tab.title}`)
-            updateDiscordRPC(response)
+            if (!window.chrome.runtime.lastError) {
+                console.log(`Now tracking: ${tab.title} with id ${tab.id}`, response)
+                updateDiscordRPC(response)
+            } else {
+                console.error('fuck')
+            }
         });
     }
 });
@@ -20,17 +30,30 @@ chrome.contextMenus.onClicked.addListener(function (info, tab) {
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (changeInfo && changeInfo.status == "complete" && tabId === selectedTabId) {
         chrome.tabs.sendMessage(tabId, { data: tab, type: "tabChange" }, function (response) {
-            updateDiscordRPC(response)
+            if (!window.chrome.runtime.lastError) {
+                console.log('Tracked tab change detected', response)
+                updateDiscordRPC(response)
+            }
         });
-
     }
 });
 
-chrome.runtime.onMessage.addListener(
-    function (request) {
-        if (request.type === "pause" || request.type === "play" || request.type === "timeupdate")
-            updateDiscordRPC(request.data);
+chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
+    if (removeInfo && tabId === selectedTabId) {
+        fetch(`http://localhost:6969`, {
+            method: "DELETE"
+        }).then(() => {
+            console.log(`Stopped tracking Tab with id: ${selectedTabId}`)
+            selectedTabId = null
+        }).catch(err => console.error('gotted error: ' + err));
     }
+})
+
+chrome.runtime.onMessage.addListener(function (request) {
+    if (request.type === "pause" || request.type === "play" || request.type === "timeupdate") {
+        updateDiscordRPC(request.data);
+    }
+}
 )
 
 function updateDiscordRPC(data) {
@@ -38,5 +61,5 @@ function updateDiscordRPC(data) {
         method: "POST",
         body: JSON.stringify(data),
         mode: "no-cors"
-    });
+    }).catch((err) => console.error(err.message));
 }
--- extension/content.js
+++ extension/content.js
@@ -1,42 +1,61 @@
 //// content.js ////
-console.log("something happens")
-let video, tab
+let video;
 let counter = 5;
 // 1. Send the background a message requesting the user's data
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
-    if (request.type === "init" || request.type === "tabChange") {
-      video = document.querySelector('video');
-      if (!window.location.search.includes('v=')) {
-        video.ontimeupdate = null;
-        video.onpause = null;
-        video.onplay = null;
-        fetch(`http://localhost:6969`, {
-          method: "DELETE"
-        });
-      }
-      else {
-        video.onpause = (event) => {
-          chrome.runtime.sendMessage({ data: formatData(document, event.target), type: "pause" });
-        }
-
-        video.onplay = (event) => {
-          chrome.runtime.sendMessage({ data: formatData(document, event.target), type: "play" });
-        }
-
-        video.ontimeupdate = (event) => {
-          if (counter === 5) {
-            counter = 0;
-            chrome.runtime.sendMessage({ data: formatData(document, event.target), type: "timeupdate" });
-          } else counter++;
-        }
-
-        sendResponse(formatData(document));
-        //TODO: add a route where you can make the discord rpc go away and execute it in an if (request.type === "tabchange")
-      }
+    if (request.type === "init") {
+      removeVideo();
+      addVideo(sendResponse);
+    } else if (request.type === "tabChange") {
+      if (!window.location.search.includes('v='))
+        removeVideo(sendResponse);
+      else
+        addVideo(sendResponse);
+      //TODO: add a route where you can make the discord rpc go away and execute it in an if (request.type === "tabchange")
+    } else if (request.type === "tabRemove") {
+      removeVideo(sendResponse);
     }
+    return true;
   }
 );
+
+function addVideo(sendResponse) {
+  video = document.querySelector('video');
+
+  video.onpause = (event) => {
+    chrome.runtime.sendMessage({ data: formatData(document, event.target), type: "pause" });
+  }
+
+  video.onplay = (event) => {
+    chrome.runtime.sendMessage({ data: formatData(document, event.target), type: "play" });
+  }
+
+  video.ontimeupdate = (event) => {
+    if (counter === 5) {
+      counter = 0;
+      chrome.runtime.sendMessage({ data: formatData(document, event.target), type: "timeupdate" });
+    } else counter++;
+  }
+
+  sendResponse(formatData(document));
+  return
+}
+
+function removeVideo(sendResponse) {
+  if (video) {
+    video.ontimeupdate = null;
+    video.onpause = null;
+    video.onplay = null;
+    video = null;
+  }
+  counter = 5;
+  fetch(`http://localhost:6969`, {
+    method: "DELETE"
+  }).then(() => {
+    if (sendResponse) sendResponse({ farewell: "removed video reference" })
+  }).catch(err => console.error('gotted error: ' + err));
+}
 
 function formatData(document, newVideo) {
   let currVideo = newVideo ? newVideo : video;
