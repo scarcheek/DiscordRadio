7a650e5a8919c28664bba33790188e61e5b58f86
Loris <scarcheekttgm@gmail.com>
2021-08-15 21:14:37 +0200

    Revert "You can now remove the tracked tab on all pages, not just the tracked one"
    This reverts commit e570b6cd5a51b8e5154a63d688e74ccfb07ed3d4.

--- extension/background.js
+++ extension/background.js
@@ -1,107 +1,105 @@
 // Contextmenu shit
 let selectedTabId = null;
 chrome.contextMenus.create({
-  id: "stop",
-  title: "Remove tracked Tab from Discord RPC",
-  contexts: ["page"],
-  documentUrlPatterns: ['*://*/*'],
-  enabled: false
-})
-
-chrome.contextMenus.create({
-  id: "track",
-  title: "Track current Tab with Discord RPC",
-  contexts: ["page"],
-  documentUrlPatterns: ['https://*.youtube.com/watch?*']
+    id: "tracker",
+    title: "Track current Video with Discord RPC",
+    contexts: ["page"],
+    documentUrlPatterns: ['https://*.youtube.com/watch?*']
 });
 
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
-  if (info.menuItemId == "track") {
-    //Change tracked page
-    if (selectedTabId && selectedTabId !== tab.id) {
-      chrome.tabs.sendMessage(selectedTabId, { type: "tabRemove" }, function (response) {
-        console.log(`Stopped tracking tab with id: ${selectedTabId}`)
-      });
+    if (info.menuItemId == "tracker") {
+        // Remove tracking from page
+        if (selectedTabId && selectedTabId === tab.id) {
+            chrome.tabs.sendMessage(selectedTabId, { type: "tabRemove" }, function (response) {
+                console.log(`Stopped tracking tab with id: ${selectedTabId}`)
+
+                updateContext();
+                selectedTabId = null
+            });
+            return
+        }
+
+        //Change tracked page
+        if (selectedTabId && selectedTabId !== tab.id) {
+            chrome.tabs.sendMessage(selectedTabId, { type: "tabRemove" }, function (response) {
+                console.log(`Stopped tracking tab with id: ${selectedTabId}`)
+            });
+        }
+
+        chrome.tabs.sendMessage(tab.id, { type: "init" }, function (response) {
+            if (!window.chrome.runtime.lastError) {
+                console.log(`Now tracking: ${tab.title} with id ${tab.id}`, response)
+
+                updateContext(true)
+                selectedTabId = tab.id
+                updateDiscordRPC(response)
+            } else {
+                console.error('You need to refresh the page before trying to track it. If that doesn\'t fix it, contact Scar#9670 on Discord');
+            }
+        });
     }
-
-    chrome.tabs.sendMessage(tab.id, { type: "init" }, function (response) {
-      if (!window.chrome.runtime.lastError) {
-        console.log(`Now tracking: ${tab.title} with id ${tab.id}`, response)
-
-        addContextMenu('stop');
-        removeContextMenu('track');
-        selectedTabId = tab.id
-        updateDiscordRPC(response)
-      } else {
-        console.error('You need to refresh the page or restart your browser before using the context menu. If that doesn\'t fix it, contact Scar#9670 on Discord');
-      }
-    });
-  }
-  if (info.menuItemId === 'stop') {
-    chrome.tabs.sendMessage(selectedTabId, { type: "tabRemove" }, function (response) {
-      console.log(`Stopped tracking tab with id: ${selectedTabId}`)
-
-      addContextMenu('track');
-      removeContextMenu('stop');
-      selectedTabId = null
-    });
-  }
 });
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (changeInfo && changeInfo.status == "complete" && tabId === selectedTabId) {
-    chrome.tabs.sendMessage(tabId, { data: tab, type: "tabChange" }, function (response) {
-      if (!window.chrome.runtime.lastError) {
-        console.log('Tracked tab change detected', response)
-        updateDiscordRPC(response)
-      }
-    });
-  }
+    if (changeInfo && changeInfo.status == "complete" && tabId === selectedTabId) {
+        chrome.tabs.sendMessage(tabId, { data: tab, type: "tabChange" }, function (response) {
+            if (!window.chrome.runtime.lastError) {
+                console.log('Tracked tab change detected', response)
+                updateDiscordRPC(response)
+            }
+        });
+    }
 });
 
 chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
-  if (removeInfo && tabId === selectedTabId) {
-    fetch(`http://localhost:6969`, {
-      method: "DELETE"
-    }).then(() => {
-      console.log(`Stopped tracking Tab with id: ${selectedTabId}`)
-      selectedTabId = null
+    if (removeInfo && tabId === selectedTabId) {
+        fetch(`http://localhost:6969`, {
+            method: "DELETE"
+        }).then(() => {
+            console.log(`Stopped tracking Tab with id: ${selectedTabId}`)
+            selectedTabId = null
 
-      addContextMenu('track');
-      removeContextMenu('stop');
-    }).catch(err => console.error('gotted error: ' + err));
-  }
+            updateContext();
+        }).catch(err => console.error('gotted error: ' + err));
+    }
 });
 
 chrome.tabs.onActiveChanged.addListener(function (tabId, selectInfo) {
-  if (selectInfo) {
-    if (tabId !== selectedTabId) {
-      addContextMenu('track');
-      return
+    if (selectInfo) {
+        if (tabId !== selectedTabId) {
+            return updateContext();
+        }
+        return updateContext(true);
     }
-    addContextMenu('stop');
-    removeContextMenu('track');
-  }
 
 });
 
 chrome.runtime.onMessage.addListener(function (request) {
-  if (request.type === "pause" || request.type === "play" || request.type === "timeupdate") {
-    updateDiscordRPC(request.data);
-  }
+    if (request.type === "pause" || request.type === "play" || request.type === "timeupdate") {
+        updateDiscordRPC(request.data);
+    }
 });
 
 function updateDiscordRPC(data) {
-  fetch(`http://localhost:6969`, {
-    method: "POST",
-    body: JSON.stringify(data),
-    mode: "no-cors"
-  }).catch((err) => console.error(err.message));
+    fetch(`http://localhost:6969`, {
+        method: "POST",
+        body: JSON.stringify(data),
+        mode: "no-cors"
+    }).catch((err) => console.error(err.message));
 }
 
-function addContextMenu(contextMenuId) {
-  chrome.contextMenus.update(contextMenuId, {enabled: true})
-}
-function removeContextMenu(contextMenuId) {
-  chrome.contextMenus.update(contextMenuId, {enabled: false})
+function updateContext(remove) {
+    if (remove) {
+        return chrome.contextMenus.update("tracker", {
+            title: "Remove current Tab from Discord RPC",
+            contexts: ["page"],
+            documentUrlPatterns: ['*://*/*']
+        })
+    }
+    return chrome.contextMenus.update("tracker", {
+        title: "Track current Video with Discord RPC",
+        contexts: ["page"],
+        documentUrlPatterns: ['https://*.youtube.com/watch?*']
+    })
 }
