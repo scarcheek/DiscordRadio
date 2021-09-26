5e9806d86841fda121fed0db0bbafb48b621525c
Lancear <Lancear153@gmail.com>
2021-09-26 12:57:06 +0200

    stats route

--- server/server.js
+++ server/server.js
@@ -25,6 +25,10 @@ setTimeout(() => {
 const httpServer = express();
 httpServer.use(express.static('public'));
 
+httpServer.get('/stats', (req, res) => {
+  res.status(200).json(stats);
+});
+
 httpServer.post('/auth', async (req, res) => {
   if (!req.body?.code && !req.body?.refresh_token) return res.status(401).json({ 
     err: 'Unauthorized, The request has not been applied because it lacks valid authentication credentials for the target resource.'
