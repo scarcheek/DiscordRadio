2bf398af29fa0e3d898a4211ca3ff75c2b37761a
Lance J <lancear153@gmail.com>
2022-04-02 16:45:56 +0200

    Fck all component libs

--- server/App.js
+++ server/App.js
@@ -1,44 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Slider } from '@mui/material';
-
-const marks = [
-  {
-    value: 0,
-    label: '0°C',
-  },
-  {
-    value: 20,
-    label: '20°C',
-  },
-  {
-    value: 37,
-    label: '37°C',
-  },
-  {
-    value: 100,
-    label: '100°C',
-  },
-];
-
-function valuetext(value) {
-  return `${value}°C`;
-}
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Text>Open up App.js to start working on your app!</Text>
-      <Slider
-        aria-label="Custom marks"
-        defaultValue={20}
-        getAriaValueText={valuetext}
-        step={10}
-        valueLabelDisplay="auto"
-        marks={marks}
-      />
+      <Button title='Lmao!'/>
     </View>
   );
 }
